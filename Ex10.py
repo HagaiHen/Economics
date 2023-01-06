08a3bc38f070e079bed14ac6d37ed8da0c8da539
hagai_hen <hagai349@gmail.com>
2023-01-06 12:16:48 +0200

    Ex10

---
+++ Ex10.py
@@ -0,0 +1,70 @@
+
+def median (list):
+    return list[int(len(list)/2)]
+
+def add2votes(c, t, n):
+    ans = []
+    for i in range(n-1):
+        ans.append((i+1)*t*c)
+    return ans
+
+
+def fun (list1, list2, list3, budget):
+    iter = 0
+    left = 0
+    right = 1
+
+    t = (left+right)/2
+    sumOfMedians = 0
+
+    OriginalList1 = list(list1)
+    OriginalList2 = list(list2)
+    OriginalList3 = list(list3)
+
+    while sumOfMedians != budget:
+
+        print("\n ======================================= Iter # " + str(iter) +"\n")
+        print("t = ", t)
+        votes = add2votes(budget, t, 3)
+        print("Added Votes ", votes)
+        for i in range(2):
+            list1.append(votes[i])
+            list2.append(votes[i])
+            list3.append(votes[i])
+
+        list1.sort()
+        list2.sort()
+        list3.sort()
+
+        print("List 1: " ,list1)
+        print("List 2: ", list2)
+        print("List 3: ", list3)
+
+        sumOfMedians = median(list1) + median(list2) + median(list3)
+
+        print("Sum of Medians: ", sumOfMedians)
+
+        if sumOfMedians > budget:
+            right = t
+            t = (right + left)/2
+        else:
+            if sumOfMedians < budget:
+                left = t
+                t = (right + left) / 2
+
+        list1 = list(OriginalList1)
+        list2 = list(OriginalList2)
+        list3 = list(OriginalList3)
+        iter += 1
+
+    return t
+
+
+
+
+
+if __name__ == '__main__':
+    list1 = [30, 0, 0]
+    list2 = [0, 15, 15]
+    list3 = [0, 15, 15]
+    fun(list1, list2, list3, 30)
